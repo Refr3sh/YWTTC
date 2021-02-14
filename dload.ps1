52d7cc0598d02b7f4f28b8eccbbfe0d520d1f810
Refr3sh <71416313+Refr3sh@users.noreply.github.com>
2021-02-14 03:26:54 +0000

    Update dload.ps1

--- dload.ps1
+++ dload.ps1
@@ -1,10 +1,10 @@
 $web = New-Object System.Net.WebClient
 $DownloadLoc = "$env:USERPROFILE\AppData\Local\DCode"
 ##DownloadFolder
-$ChkDownloadLoc = Test-Path $DownloadLoc
+$ChkDownloadLoc = Test-Path "$DownloadLoc"
 If(!($ChkDownloadLoc -eq $True)){
-	New-Item -Path $DownloadLoc -ItemType Directory -Force
+	New-Item -Path "$DownloadLoc" -ItemType Directory -Force
 	New-Item -Path "$DownloadLoc\Font" -ItemType Directory -Force
 }
 $web.DownloadString('https://raw.githubusercontent.com/Refr3sh/YWTTC/main/Main.ps1') > "$DownloadLoc\Main.ps1"
-&"$Env:UserProfile\Appdata\Local\DCode\Main.ps1" -NoExit
+&"$Env:UserProfile\Appdata\Local\DCode\Main.ps1"
