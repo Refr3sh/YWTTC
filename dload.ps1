62046839af63327d781bdd47a5ca0bb5cd287cac
Refr3sh <71416313+Refr3sh@users.noreply.github.com>
2021-02-14 03:23:51 +0000

    Update dload.ps1

--- dload.ps1
+++ dload.ps1
@@ -6,5 +6,5 @@ If(!($ChkDownloadLoc -eq $True)){
 	New-Item -Path $DownloadLoc -ItemType Directory -Force
 	New-Item -Path "$DownloadLoc\Font" -ItemType Directory -Force
 }
-$web.DownloadString("https://raw.githubusercontent.com/Refr3sh/YWTTC/main/Main.ps1") > "$DownloadLoc\Quiet.ps1"
-&"env:UserProfile\Appdata\Local\DCode\Quiet.ps1" -NoExit
+$web.DownloadString('https://raw.githubusercontent.com/Refr3sh/YWTTC/main/Main.ps1') > "$DownloadLoc\Main.ps1"
+&"$Env:UserProfile\Appdata\Local\DCode\Quiet.ps1" -NoExit
