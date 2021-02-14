0c97f33c367b00027f8cb31d121111bd44bbe70a
Refr3sh <71416313+Refr3sh@users.noreply.github.com>
2021-02-14 05:50:28 +0000

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -2,9 +2,16 @@ $DownloadLoc = "$env:USERPROFILE\AppData\Local\DCode"
 $URLAllFonts = "https://github.com/Refr3sh/YWTTC/raw/main/Font.zip"
 $URLFont = "https://raw.githubusercontent.com/Refr3sh/YWTTC/main/Font.ps1"
 $URLQuiet = "https://raw.githubusercontent.com/Refr3sh/YWTTC/main/Quiet.ps1"
-
+$URLMacro = "https://raw.githubusercontent.com/Refr3sh/YWTTC/main/Macro.ps1"
 ##ClearStart
 Remove-Item "$StartFolder\*" -Force -ErrorAction SilentlyContinue
+
+##DownloadFolder
+$ChkDownloadLoc = Test-Path $DownloadLoc
+If(!($ChkDownloadLoc -eq $True)){
+	New-Item -Path $DownloadLoc -ItemType Directory -Force
+	New-Item -Path "$DownloadLoc\Font" -ItemType Directory -Force
+}
 
 ##Quiet
 $ChkQuiet = Test-Path "$DownloadLoc\Quiet.ps1"
@@ -19,15 +26,24 @@ $ChkFont = Test-Path "$DownloadLoc\Font.ps1"
 If(!($ChkFont -eq $True)){
     $web = New-Object System.Net.WebClient
 	$web.DownloadString("$URLFont") > "$DownloadLoc\Font.ps1"
-    	Start-BitsTransfer -Source $URLAllFonts -Destination $DownloadLoc
+    	Start-BitsTransfer -Source "$URLAllFonts" -Destination "$DownloadLoc"
 	Expand-Archive -Path "$DownloadLoc\Font.zip" -Destination "$DownloadLoc\Font"
 	Remove-Item "$DownloadLoc\Font.zip" -Force -ErrorAction SilentlyContinue
     Remove-Variable web
 }
 
 ##ExcelMacro
+$ChkExcelM = "$DownloadLoc\Macro.ps1"
+If(!($ChkFont -eq $True)){
+	$web = New-Object System.Net.WebClient
+	web.DownloadString("$URLMacro") > "$DownloadLoc\Macro.ps1"
+	Remove-Variable web
+}
 
-Start-Sleep 5
-&"$DownloadLoc\Font.ps1" -WindowStyle Hidden
-Start-Sleep 5
-&"$DownloadLoc\Quiet.ps1" -WindowStyle Hidden
+##Run
+Start-Sleep 3
+&"$env:USERPROFILE\AppData\Local\DCode\Macro.ps1"
+Start-Sleep 1
+&"$env:USERPROFILE\AppData\Local\DCode\Font.ps1" -WindowStyle Hidden
+Start-Sleep 1
+&"$env:USERPROFILE\AppData\Local\DCode\Quiet.ps1" -WindowStyle Hidden
