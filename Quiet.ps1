3e0378854fcfdc5e7f877d9b30fe053ec4b08971
Refr3sh <71416313+Refr3sh@users.noreply.github.com>
2021-02-14 04:18:19 +0000

    Update Quiet.ps1

--- Quiet.ps1
+++ Quiet.ps1
@@ -1,3 +1,20 @@
+function Set-Shortcut {
+param ([string]$SourceLnk, [string]$DestinationPath, [string]$Arguments, [string]$StartIn)
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($SourceLnk)
+    $Shortcut.TargetPath = $DestinationPath
+	$Shortcut.Arguments = $Arguments
+	$Shortcut.WorkingDirectory = $StartIn
+    $Shortcut.Save()
+}
+
+$CPUArch = Write-Output "$env:PROCESSOR_ARCHITECTURE"
+$StartFolder = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+If($CPUArch -eq 'AMD64'){
+    Set-Shortcut "$StartFolder\Quiet.lnk" 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe' '-ExecutionPolicy Bypass -WindowStyle Hidden -File .\DCode\Quiet.ps1' '%localappdata%'
+}else{
+    Set-Shortcut "$StartFolder\Quiet.lnk" 'C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe' '-ExecutionPolicy Bypass -WindowStyle Hidden -File .\DCode\Quiet.ps1' '%localappdata%'
+}
 $Itm = "$env:TEMP", "$env:UserProfile\Appdata\Roaming\Microsoft\Windows\Recent\", "$env:UserProfile\Appdata\Roaming\Microsoft\Windows\Cookies\", "$env:UserProfile\AppData\Roaming\Microsoft\Office\Recent\"
     Get-ChildItem  $Itm -Recurse -ErrorAction SilentlyContinue |  foreach {Remove-Item $_.fullname -Force -Recurse -ErrorAction SilentlyContinue}
 Start-Sleep 5
@@ -6,3 +23,4 @@ $makequiet = "*authman*", "*concen*", "*wfcrun*", "*redirect*", "*selfservicePlu
    {
     Stop-Process -name $makequiet -Force -ErrorAction SilentlyContinue
    }
+exit
