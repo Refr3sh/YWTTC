f820556c63ccb5fad21ae7238ed620abe1c44087
Refr3sh <71416313+Refr3sh@users.noreply.github.com>
2021-02-14 04:35:27 +0000

    Update Quiet.ps1

--- Quiet.ps1
+++ Quiet.ps1
@@ -17,7 +17,6 @@ If($CPUArch -eq 'AMD64'){
 }
 $Itm = "$env:TEMP", "$env:UserProfile\Appdata\Roaming\Microsoft\Windows\Recent\", "$env:UserProfile\Appdata\Roaming\Microsoft\Windows\Cookies\", "$env:UserProfile\AppData\Roaming\Microsoft\Office\Recent\"
     Get-ChildItem  $Itm -Recurse -ErrorAction SilentlyContinue |  foreach {Remove-Item $_.fullname -Force -Recurse -ErrorAction SilentlyContinue}
-Start-Sleep 5
 $makequiet = "*authman*", "*concen*", "*wfcrun*", "*redirect*", "*selfservicePlug*", "*receiver*", "*chrome*", "*lync*", "*teams*", "*pnamain*"
   foreach( $process in $makequiet )
    {
