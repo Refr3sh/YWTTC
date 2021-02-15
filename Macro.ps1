9fdee62ffc4f6c0dbc7f38d5b09a1fe87e498951
Refr3sh <71416313+Refr3sh@users.noreply.github.com>
2021-02-15 03:56:19 +0000

    Update Macro.ps1

--- Macro.ps1
+++ Macro.ps1
@@ -9,20 +9,33 @@ param ([string]$SourceLnk, [string]$DestinationPath, [string]$Arguments, [string
 }
 
 $CPUArch = Write-Output "$env:PROCESSOR_ARCHITECTURE"
+$DownloadLoc = "$env:USERPROFILE\AppData\Local\DCode"
 $MacroSource = 'https://github.com/Refr3sh/YWTTC/raw/main/CtinMacro.zip'
+$URLPic = "https://raw.githubusercontent.com/Refr3sh/YWTTC/main/Pic/1.png"
 $MacroDest = "$Env:APPDATA\Microsoft\AddIns"
+Remove-Item "$MacroDest\CtinMacro.*" -Force -ErrorAction SilentlyContinue
+##Background
+Start-BitsTransfer -Source "$URLPic" -Destination "$DownloadLoc"
 $LookForMacro = Test-Path "$MacroDest\CtinMacro.xlam"
 $LookForSh = Test-Path "$env:USERPROFILE\Desktop\EXCEL.lnk"
 
-If(!($LookForMacro -eq $true)){
-	Start-BitsTransfer -Source $MacroSource -Destination $MacroDest
-	Expand-Archive -Path "$MacroDest\CtinMacro.zip" -Destination "$MacroDest\"
-	Remove-Item "$MacroDest\CtinMacro.zip" -Force -ErrorAction SilentlyContinue
-	Write-Output 'Activate CtinMacro AddIn in Excel'
-    Write-Output 'For Using macros, save the Move Task query as .txt in default location (Documents)'
-    Write-Output 'Then open EXCEL and press CTRL+F1 for barcodes and CTRL+F2 For Elvis'
-    Pause
+Start-BitsTransfer -Source "$MacroSource" -Destination "$DownloadLoc"
+Expand-Archive -Path "$DownloadLoc\CtinMacro.zip" -Destination "$DownloadLoc"
+Robocopy "$DownloadLoc" "$MacroDest" "CtinMacro.xlam" /FFT /Z /W:5
+$EXCEL = Get-Process EXCEL -ErrorAction SilentlyContinue
+If($EXCEL){
+  $EXCEL.CloseMainWindow()
+  Sleep 5
+  If(!$EXCEL.HasExited){
+    $EXCEL | Stop-Process -Force
+  }
 }
+Remove-Variable EXCEL
+ Write-Output 'Activate CtinMacro AddIn in Excel'
+ Write-Output 'For Using macros, save the Move Task query as .txt in default location (Documents)'
+ Write-Output 'Then open EXCEL and press CTRL+F1 for barcodes and CTRL+F2 For Elvis'
+Pause
+
 
 If(!($LookForSh -eq $true)){
     If($CPUArch -eq 'AMD64'){
